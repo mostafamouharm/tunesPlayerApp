a2cc7be717b28e114363c7c683779a98cc484b53
mostafamouharm <moustafa.mo7arm@gmail.com>
2023-08-22 19:22:45 +0300

    Tunes App

--- lib/views/tune_page.dart
+++ lib/views/tune_page.dart
@@ -3,7 +3,15 @@ import 'package:tunes_player_app/widgets/tune_item.dart';
 
 class TuneView extends StatelessWidget {
   const TuneView({super.key});
-
+  final List<Color> tunesColors = const [
+    Color(0xffF44336),
+    Color(0xffF89800),
+    Color(0xffFEEB3B),
+    Color(0xff4CAF50),
+    Color(0xff2F9688),
+    Color(0xff2896F3),
+    Color(0xff9C27B0),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,32 +24,22 @@ class TuneView extends StatelessWidget {
           style: TextStyle(),
         ),
       ),
-      body: const Column(
-        children: [
-          TuneItem(
-            color: Colors.red,
-          ),
-          TuneItem(
-            color: Colors.black,
-          ),
-          TuneItem(
-            color: Colors.blue,
-          ),
-          TuneItem(
-            color: Colors.white,
-          ),
-          TuneItem(
-            color: Colors.green,
-          ),
-          TuneItem(
-            color: Colors.yellow,
-          ),
-          TuneItem(
-            color: Colors.orange,
-          ),
-          
-        ],
+      body: Column(
+        children: tunesColors
+            .map(
+              (e) => TuneItem(color: e),
+            )
+            .toList(),
       ),
     );
   }
+
+  // List<TuneItem> getTuneItems() {
+  //   List<TuneItem> items = [];
+
+  //   for (var color in tunesColors) {
+  //     items.add(TuneItem(color: color));
+  //   }
+  //   return items;
+  // }
 }
